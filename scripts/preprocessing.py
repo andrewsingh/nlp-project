e0121fb567cafe362ee8f7015a3028c6fd455f64
Andrew Singh <andrew.singh99@gmail.com>
2019-10-07 10:56:36 -0400

    Added data and preprocessing script

---
+++ scripts/preprocessing.py
@@ -0,0 +1,25 @@
+import spacy
+from tabulate import tabulate
+
+
+# path: path to text file
+# analysis: 0 for general preprocessing, 1 for named entity recognition
+# example: preprocess("Development_data/set1/a1.txt", 1)
+def preprocess(path, analysis=0):
+	with open(path, "r") as file:
+		text = file.read()
+		nlp = spacy.load("en_core_web_sm")
+		doc = nlp(text)
+		results = []
+		if analysis == 0:
+			for token in doc:
+					results.append([token.text, token.lemma_, token.pos_, token.tag_, \
+						token.dep_, token.shape_, token.is_alpha, token.is_stop])
+		elif analysis == 1:
+			for ent in doc.ents:
+				results.append([ent.text, ent.start_char, ent.end_char, ent.label_])
+		print(tabulate(results))
+		return doc
+
+
+
